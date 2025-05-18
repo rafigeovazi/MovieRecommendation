d21ca16c174c782da39406bc4190ece0f21708fd
Geosling <145568712+rafigeovazi@users.noreply.github.com>
2025-05-18 22:42:33 +0700

    Update app2.py

--- app2.py
+++ app2.py
@@ -12,8 +12,7 @@ if "logged_in" not in st.session_state:
 if not st.session_state.logged_in:
     logged_in = login()
     if logged_in:
-        st.experimental_rerun()
-        st.session_state.logged_in = True
+       st.stop()
 else:
     # -----------------------------
     # ✅ Main Page
